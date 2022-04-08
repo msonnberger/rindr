d82dbe81a75aee0683857e9c6e1955e4272984ea
Juliane Mohr <julianemohr@hotmail.com>
2022-04-08 09:19:49 +0200

    add colors file and icons to navbar

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,5 @@
 import Navbar from './navbar'
+
 export default function Footer() {
   return (
     <Navbar />
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,18 @@
+import { HomeIcon } from './icons/homeIcon'
+import { ChatIcon } from './icons/chatIcon'
+import { PigIcon } from './icons/pigIcon'
+import { RidesCarIcon } from './icons/ridesCarIcon'
+import { ProfileIcon } from './icons/profileIcon'
+import { Slate } from '../styles/colors'
+
 export default function Navbar() {
   return (
-    <div className={"flex flex-row justify-evenly w-full bg-slate-50 h-16 rounded-3xl"}>
-      <p>h</p>
-      <p>h</p>
+    <div className={"flex flex-row justify-evenly w-full bg-slate-50 h-16 rounded-3xl items-center"}>
+      <HomeIcon color={Slate[300]}/>
+      <ChatIcon color={Slate[300]}/>
+      <PigIcon color={Slate[300]}/>
+      <RidesCarIcon color={Slate[300]}/>
+      <ProfileIcon color={Slate[300]}/>
     </div>
   )
 }
