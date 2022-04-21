f8a3dffb812085ece79469e5882976c432860fd6
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2022-04-21 18:59:04 +0200

    add TextInput component

---
+++ src/components/inputs/textInput.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { UseFormRegister } from 'react-hook-form'
+
+interface TextInputProps {
+  placeholder: string
+  name: string
+  icon?: React.ReactNode
+  tailwindBgClass?: string
+  register: UseFormRegister<any>
+}
+
+export default function TextInput({ placeholder, icon, tailwindBgClass, name, register }: TextInputProps) {
+  return (
+    <div className="flex gap-4 rounded-full bg-slate-100 p-2 pr-8">
+      {icon && <div className={`${tailwindBgClass} grid h-8 w-8 place-items-center rounded-full`}>{icon}</div>}
+      <input type="text" id={name} placeholder={placeholder} {...register(name)} className="bg-slate-100" />
+    </div>
+  )
+}
--- src/components/setupProfile.tsx
+++ src/components/setupProfile.tsx
@@ -1,6 +1,9 @@
 import { useForm, SubmitHandler } from 'react-hook-form'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleUser, faPlus, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { TextInput } from '@components/inputs'
 
-interface Inputs {
+interface SetupProfileFormValues {
   picture: FileList
   location: string
   bio: string
@@ -12,15 +15,16 @@ interface Inputs {
 }
 
 export default function SetupProfile() {
-  const { register, handleSubmit, watch } = useForm<Inputs>()
+  const { register, handleSubmit, watch } = useForm<SetupProfileFormValues>()
   const watchPicture = watch('picture')
-  let picturePreview
+  let picturePreview = ''
 
-  if (watchPicture) {
+  if (watchPicture && watchPicture[0]) {
+    console.log(typeof watchPicture[0])
     picturePreview = URL.createObjectURL(watchPicture[0])
   }
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
+  const onSubmit: SubmitHandler<SetupProfileFormValues> = (data) => {
     console.log(data)
   }
 
@@ -28,12 +32,25 @@ export default function SetupProfile() {
     <div className="flex flex-col items-start">
       <h1 className="text-4xl font-bold text-rose-500">Welcome, Martin!</h1>
       <h2 className="mt-3 text-lg">Please finish setting up your profile.</h2>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center">
-        <img src={picturePreview} alt="Profile picture" className="h-48 w-48 rounded-full" />
-        <input id="picture" type="file" className="hidden" {...register('picture')} />
-        <label htmlFor="picture">Upload Photo</label>
-
-        <input type="text" id="location" placeholder="Where do you commute from?" {...register('location')} />
+      <form onSubmit={handleSubmit(onSubmit)} className="mt-8 flex flex-col items-center gap-5 border-2 border-red-500">
+        <label className="relative cursor-pointer" htmlFor="picture">
+          {picturePreview ? (
+            <img src={picturePreview} alt="Profile picture" className="h-48 w-48 rounded-full" />
+          ) : (
+            <FontAwesomeIcon icon={faCircleUser} className="text-[12rem] text-rose-200" />
+          )}
+          <input id="picture" type="file" accept="image/*" className="hidden" {...register('picture')} />
+          <div className="absolute bottom-3 right-2 grid h-7 w-7 place-items-center rounded-full border-2 border-white bg-rose-500">
+            <FontAwesomeIcon icon={faPlus} className="text-md text-white" />
+          </div>
+        </label>
+        <TextInput
+          register={register}
+          name="location"
+          tailwindBgClass="bg-rose-500"
+          icon={<FontAwesomeIcon icon={faLocationDot} color="white" />}
+          placeholder="Where do you commute from?"
+        />
         <input type="textarea" id="bio" placeholder="Tell us something about you" {...register('bio')} />
 
         <div>
