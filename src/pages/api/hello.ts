5857f685b814f4f36a334d215d9534c4fccef6a7
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2022-05-02 17:31:59 +0200

    add distance function

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,26 +1,49 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
 
-type Data = {
-  name: string
+type Coordinates = {
+  latitude: number
+  longitude: number
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getSession({ req })
+
+  if (!session) {
+    res.status(500)
+    return
+  }
+
+  const from = { longitude: 13.254923393796085, latitude: 47.98067277832721 }
+  const to = { longitude: 13.086791861894637, latitude: 47.723227207131735 }
+  const via = { longitude: session.user.longitude, latitude: session.user.latitude }
+
+  const extraDistance = await calculateExtraDistanceNeeded(47000, from, to, via)
+
+  res.status(200).json({ extraDistance })
 }
 
-const from = '47.98067277832721, 13.254923393796085'
-const to = '47.723227207131735, 13.086791861894637'
-const viapoint = '47.868879712358044, 13.128797945605017'
-const mapbox = `https://api.mapbox.com/directions/v5/mapbox/driving/${from};${viapoint};${to}?geometries=geojson&access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`
-const time = '2022-05-02T15:00'
+function combineCoordinates(locations: Coordinates[]) {
+  const stringLocations = locations.map((location) => `${location.longitude},${location.latitude}`)
+  return stringLocations.join(';')
+}
 
-async function fetchRoute() {
-  const res = await fetch(
-    `https://api.mapbox.com/directions/v5/mapbox/driving/-84.518641,39.134270;-84.512023,39.102779?geometries=geojson&access_token=${process.env.NEXT_PUBLIC_MAPBOX_KEY}&depart_at=${time}`
-  )
-  const response = await res.json()
-  setRoute(response)
-  console.log('ERRRROOOOR')
-  console.log(route)
+async function calculateExtraDistanceNeeded(
+  distance: number,
+  from: Coordinates,
+  to: Coordinates,
+  via: Coordinates
+) {
+  const baseUrl = `https://api.mapbox.com/directions/v5/mapbox/driving/`
+
+  const params = new URLSearchParams([
+    ['geometries', 'geojson'],
+    ['access_token', process.env.PRIVATE_MAPBOX_KEY as string],
+  ])
+
+  const url = `${baseUrl}${combineCoordinates([from, via, to])}?${params.toString()}`
+  const response = await fetch(url)
+  const data = await response.json()
+
+  return (data.routes[0].distance - distance) / 1000
 }
