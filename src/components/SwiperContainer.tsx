a67a407d49d7ab7fe1908e8ee8e65f27cd18a4bb
Juliane Mohr <julianemohr@hotmail.com>
2022-05-03 14:15:08 +0200

    Bug: state does not update2

--- src/components/SwiperContainer.tsx
+++ src/components/SwiperContainer.tsx
@@ -1,10 +1,32 @@
 import { faRoute } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
+import TinderCard from 'react-tinder-card'
+import { SwiperCard } from './SwiperCard'
 
 interface SwiperContainerProps {
   setOpenFilter: any
 }
 export const SwiperContainer = ({ setOpenFilter }: SwiperContainerProps) => {
+  const [newRequests, setNewRequests] = useState(1)
+  const [swipeRight, setSwipeRight] = useState('test')
+
+  const onSwipe = (direction: string, index: number) => {
+    console.log('You swiped: ' + direction)
+    if (direction === 'right') {
+      //debugger
+      setNewRequests(2)
+      //setNewRequests([index, ...newRequests])
+      setSwipeRight('hallo')
+      console.log('RIGHT', index)
+    }
+  }
+
+  const onCardLeftScreen = (myIdentifier: string) => {
+    console.log(myIdentifier + ' left the screen')
+    console.log(newRequests, 'state')
+    console.log(swipeRight, 'swipeRight')
+  }
   return (
     <>
       <button
@@ -14,6 +36,24 @@ export const SwiperContainer = ({ setOpenFilter }: SwiperContainerProps) => {
         <FontAwesomeIcon icon={faRoute} size="lg" />
         <p className="ml-3 font-bold">Filter</p>
       </button>
+      <div className="relative flex justify-center mt-4">
+        <TinderCard
+          className="absolute z-0"
+          onSwipe={(dir) => onSwipe(dir, 1)}
+          onCardLeftScreen={() => onCardLeftScreen('1')}
+          preventSwipe={['up', 'down']}
+        >
+          <SwiperCard />
+        </TinderCard>
+        <TinderCard
+          className="absolute z-0"
+          onSwipe={(dir) => onSwipe(dir, 2)}
+          onCardLeftScreen={() => onCardLeftScreen('2')}
+          preventSwipe={['up', 'down']}
+        >
+          <SwiperCard />
+        </TinderCard>
+      </div>
     </>
   )
 }
