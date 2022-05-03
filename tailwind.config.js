8200bb083bb8cc04f68a85118a6a3caa4c76f3ed
Juliane Mohr <julianemohr@hotmail.com>
2022-05-03 12:09:48 +0200

    Add styling share ride/find ride buttons in index.tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,14 +11,23 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      height: {
+        '600px': '600px',
+      },
       animation: {
         slideIn: '1s ease-out 0s 1 slideIn',
+      },
+      width: {
+        'fit-content': 'fit-content',
       },
       keyframes: {
         slideIn: {
           '0%': { transform: 'translateX(-30%)' },
           '100%': { transform: 'translateX(0%)' },
         },
+      },
+      borderRadius: {
+        '4xl': '50px',
       },
     },
   },
