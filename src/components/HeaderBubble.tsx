9fa54d555ef974d9984073b4029245b3e8c13a45
Maximilian Hajek <mhajek.mmt-b2020@fh-salzburg.ac.at>
2022-05-05 10:18:52 +0200

    Styling fix

--- src/components/HeaderBubble.tsx
+++ src/components/HeaderBubble.tsx
@@ -10,7 +10,7 @@ interface HeaderBubbleProps {
 const HeaderBubble = ({ color, text, path, alt }: HeaderBubbleProps) => {
   return (
     <div
-      className={`absolute top-0 -ml-5 w-full max-w-4xl flex justify-center items-center gap-x-6 h-16 rounded-b-2xl ${color} px-8`}
+      className={`absolute z-10 top-0 -ml-5 w-full max-w-4xl flex justify-center items-center gap-x-6 h-16 rounded-b-2xl ${color} px-8`}
     >
       <Image src={path} alt={alt} width="32" height="32" />
       <p className="text-sm font-semibold">{text}</p>
