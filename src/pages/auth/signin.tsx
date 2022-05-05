3491ee3fb24ff6212b0ede9f606c490497535fee
Maximilian Hajek <mhajek.mmt-b2020@fh-salzburg.ac.at>
2022-05-05 12:05:43 +0200

    Fix login redirection bug

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,31 +1,21 @@
 import { NextPageContext } from 'next'
-import { getCsrfToken } from 'next-auth/react'
+import { signIn } from 'next-auth/react'
 
-interface SignInProps {
-  csrfToken: string
+interface SignInProp {
   callbackUrl: string
 }
 
-export default function SignIn({ csrfToken, callbackUrl }: SignInProps) {
+export default function SignIn({ callbackUrl }: SignInProp) {
   return (
-    <main className="grid place-items-center min-h-screen">
-      <form action="/api/auth/signin/fhs" method="POST">
-        <input type="hidden" name="csrfToken" value={csrfToken} />
-        <input type="hidden" name="callbackUrl" value={callbackUrl} />
-        <input
-          type="submit"
-          className="cursor-pointer rounded-full bg-rose-500 py-3 px-5 font-semibold text-rose-50"
-          value="Sign in with FH Salzburg"
-        />
-      </form>
-    </main>
+    <button onClick={() => signIn('fhs', { callbackUrl: callbackUrl })}>
+      Sign in with FH Salzburg
+    </button>
   )
 }
 
 export async function getServerSideProps(context: NextPageContext) {
-  const csrfToken = await getCsrfToken(context)
   const { callbackUrl } = context.query
   return {
-    props: { csrfToken, callbackUrl },
+    props: { callbackUrl },
   }
 }
