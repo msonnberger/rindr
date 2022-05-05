3446d91464c7ec94325c2cbe71d4a8be42a114e5
Juliane Mohr <julianemohr@hotmail.com>
2022-05-05 16:39:07 +0200

    Outline none textarea

--- src/components/inputs/TextareaInput.tsx
+++ src/components/inputs/TextareaInput.tsx
@@ -11,7 +11,7 @@ export default function TextInput({ placeholder, id, register }: TextInputProps)
     <div className={`flex gap-4 rounded-3xl bg-slate-100 p-2 w-full h-32 `}>
       <textarea
         id={id}
-        className="bg-slate-100 w-full h-full resize-none p-2"
+        className="bg-slate-100 w-full h-full resize-none p-2 outline-none"
         placeholder={placeholder}
         {...register('bio', {
           required:
