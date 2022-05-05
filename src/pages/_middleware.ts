c97ea8aa06c4b7be328fefa4ce7a822d62a3dad4
Pascal Gottschling <fhs45880@fh-salzburg.ac.at>
2022-05-05 11:03:13 +0200

    add if bugfix new user

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -27,9 +27,11 @@ async function middleware(request: NextAuthRequest) {
 
   if (!profileSetupCompleted) {
     const userData = await fetchUserData(sub as string)
+    console.log(userData)
 
-    if (!userData) {
+    if (!userData?.id) {
       url.pathname = '/auth/newuser'
+      console.log('bin ')
       return NextResponse.redirect(url)
     }
   }
