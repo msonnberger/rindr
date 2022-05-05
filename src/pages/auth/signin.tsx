042f01a1feb39494ca9c2fdf1872044f31a2d6e7
Maximilian Hajek <mhajek.mmt-b2020@fh-salzburg.ac.at>
2022-05-05 12:15:27 +0200

    Update styling

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -7,9 +7,14 @@ interface SignInProps {
 
 export default function SignIn({ callbackUrl }: SignInProps) {
   return (
-    <button onClick={() => signIn('fhs', { callbackUrl: callbackUrl })}>
-      Sign in with FH Salzburg
-    </button>
+    <main className="grid place-items-center min-h-screen">
+      <button
+        onClick={() => signIn('fhs', { callbackUrl: callbackUrl })}
+        className="cursor-pointer rounded-full bg-rose-500 py-3 px-5 font-semibold text-rose-50"
+      >
+        Sign in with FH Salzburg
+      </button>
+    </main>
   )
 }
 
