7fc10b57a818002298de933f6324130faeeff56c
Pascal Gottschling <fhs45880@fh-salzburg.ac.at>
2022-05-06 10:48:52 +0200

    fix test for react 18

--- __tests__/button.test.js
+++ __tests__/button.test.js
@@ -1,47 +1,21 @@
+import '@testing-library/jest-dom'
 import { fireEvent, queryByText, render, screen } from '@testing-library/react'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client'
 import Button from '../src/components/Button.tsx'
 
-let container
-
-beforeEach(() => {
-  container = document.createElement('div')
-  document.body.appendChild(container)
-})
-
-afterEach(() => {
-  document.body.removeChild(container)
-  container = null
-})
-
 it('Test if Button is rendered', () => {
-  ReactDOM.render(
-    <Button buttonType="button" text="Done" bgColor="bg-rose-700"></Button>,
-    container
-  )
+  render(<Button buttonType="button" text="Done" bgColor="bg-rose-700"></Button>)
 
-  expect(queryByText(container, 'Done')).toBeTruthy()
+  expect(screen.getByText(/done/i)).toBeInTheDocument()
 })
-
-// it('Test if Button has correct color', () => {
-//   ReactDOM.render(
-//     <Button buttonType="button" text="Done" bgColor="bg-rose-500"></Button>,
-//     container
-//   )
-
-//   expect(container).toHaveStyle({ backgroundColor: '#f43f5e' })
-// })
 
 it('if button clicked, onClick is called', () => {
   const onClick = jest.fn()
 
-  ReactDOM.render(
-    <Button buttonType="button" text="Done" bgColor="bg-rose-700" onClick={onClick}></Button>,
-    container
-  )
-  fireEvent.click(container.querySelector('button'))
+  render(<Button buttonType="button" text="Done" bgColor="bg-rose-700" onClick={onClick}></Button>)
+  fireEvent.click(screen.getByText(/Done/i))
 
   expect(onClick).toHaveBeenCalledTimes(1)
 })
